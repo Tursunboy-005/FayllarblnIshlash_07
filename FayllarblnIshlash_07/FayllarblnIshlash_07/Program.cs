eda37c528c66ac00565f9fe369e8ae64a42bedff
Tursunboy <123323706+Tursunboy-005@users.noreply.github.com>
2023-06-08 21:42:43 +0500

    Initial commi

---
+++ FayllarblnIshlash_07/FayllarblnIshlash_07/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FayllarblnIshlash_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
